5abcfc3682b3a4670e699323e24a9668716838d4
hrdc0r3 <mordor@mordo.com>
2024-09-30 20:03:19 +0200

    ref

--- session1_3_pillars/metrics_example.py
+++ session1_3_pillars/metrics_example.py
@@ -1,16 +1,17 @@
 import time
 from opentelemetry import metrics
 from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import ConsoleMetricExporter, PeriodicExportingMetricReader
+from prometheus_client import start_http_server, Counter
 
 # Set up the meter provider
 metrics.set_meter_provider(MeterProvider())
 meter = metrics.get_meter(__name__)
 
-# Set up metric exporter
-exporter = ConsoleMetricExporter()
-reader = PeriodicExportingMetricReader(exporter)
-metrics.get_meter_provider().register_metric_reader(reader)
+# Start the Prometheus HTTP server on port 8000
+start_http_server(8000)
+
+# Create a custom Prometheus metric
+request_counter = Counter('request_counter', 'Counts the number of requests')
 
 # Create a counter to track the number of requests
 request_counter = meter.create_counter(
@@ -26,8 +27,8 @@ response_time_histogram = meter.create_histogram(
     unit="ms"
 )
 
-# Simulate a request and record metrics
-for _ in range(5):
+# Simulate a request and record prometheus_metrics
+for _ in range(50):
     request_counter.add(1)
     response_time = 100 + (time.time() % 1) * 100  # Simulate response time
     response_time_histogram.record(response_time)
